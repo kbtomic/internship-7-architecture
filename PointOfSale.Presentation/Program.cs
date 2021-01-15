15c6051a6f60eb94fef8bde3a026d467eb800e89
kbtomic <ktomic01@fesb.hr>
2021-01-15 11:09:07 +0100

    Added layers and needed packages

---
+++ PointOfSale.Presentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointOfSale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
